459d11a4a13afe290b56a8011acf7ea2494278f6
degurny <degurny@degurniy2.IL.RU>
2019-06-10 01:48:20 +0400

    read 4 variants of authentication, add User class and UserRepo inteface. Page 98

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package tacos.security;
 
+
+import javax.sql.DataSource;
+import javax.xml.stream.events.EndDocument;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,26 +18,57 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	//@SuppressWarnings("deprecation")
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
 	
-
-
+	
+	
+	
+	/* LDAP Authentication example
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)
+	throws Exception {
+	auth
+		.ldapAuthentication()
+			.userSearchBase("ou=people")
+			.userSearchFilter("(uid={0})")
+			.groupSearchBase("ou=groups")
+			.groupSearchFilter("member={0}")
+			.passwordCompare()
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.passwordAttribute("passcode");			
+	}
+	*/ // end LDAP Authentication example
+	
+	/*  starting JDBC-authentication
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.jdbcAuthentication()
+				.dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from Users where username=?")
+				.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?")
+				.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+	}
+	*/ // end JDBC-authentication
+
+
+	/*  The In-Memory Authentication
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		auth.inMemoryAuthentication()
 			.withUser("buzz")
-			.password("infinity")
+			//.password(encoder.encode("infinity"))
+			.password("{noop}infinity")
 			.authorities("ROLE_USER")
 		.and()
 			.withUser("woody")
-			.password("bullseye")
+			.password("{noop}bullseye")
 			.authorities("ROLE_USER");
 		
 	}
+	 */ //end  The In-Memory Authentication  
 
 }
